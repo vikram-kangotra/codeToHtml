dd0a4a8b74b561f9defbd61190b2cdf185d20e7f
vikram kangotra <vikramkangotra8055@gmail.com>
2021-03-01 00:25:28 +0530

    ok

--- core/token.h
+++ core/token.h
@@ -61,21 +61,37 @@ class Token
 
         std::string getTypeStr() const {
             switch(mType) {
-                case TokenType::IDENTIFIER:            return "Identifier"; break;
-                case TokenType::NUMBER:                return "Number"; break;
-                case TokenType::PLUS:                  return "Plus"; break;
-                case TokenType::MINUS:                 return "Minus"; break;
-                case TokenType::MULT:                  return "Mult"; break;
-                case TokenType::DIV:                   return "Div"; break;
-                case TokenType::GREATER_THAN:          return "Grater Than"; break;
-                case TokenType::GREATER_THAN_OR_EQUAL: return "Greater Than Or Equal"; break;
-                case TokenType::LESS_THAN:             return "Less Than"; break;
-                case TokenType::LESS_THAN_OR_EQUAL:    return "Less Than Or Equal"; break;
-                case TokenType::EQUAL:                 return "Equal"; break;
-                case TokenType::ASSIGN:                return "Assign"; break;
-                case TokenType::LEFT_PARENTHESIS:      return "Left ParenThesis"; break;
-                case TokenType::RIGHT_PARENTHESIS:     return "RightParenthesis"; break;
-                case TokenType::END_OF_INPUT:          return "End Of Input"; break;
+                case TokenType::IDENTIFIER:             return "Identifier"; break;
+                case TokenType::NUMBER:                 return "Number"; break;
+                case TokenType::PLUS:                   return "Plus"; break;
+                case TokenType::MINUS:                  return "Minus"; break;
+                case TokenType::MULT:                   return "Mult"; break;
+                case TokenType::DIV:                    return "Div"; break;
+                case TokenType::GREATER_THAN:           return "Grater Than"; break;
+                case TokenType::GREATER_THAN_OR_EQUAL:  return "Greater Than Or Equal"; break;
+                case TokenType::LESS_THAN:              return "Less Than"; break;
+                case TokenType::LESS_THAN_OR_EQUAL:     return "Less Than Or Equal"; break;
+                case TokenType::EQUAL:                  return "Equal"; break;
+                case TokenType::ASSIGN:                 return "Assign"; break;
+                case TokenType::LEFT_PARENTHESIS:       return "Left ParenThesis"; break;
+                case TokenType::RIGHT_PARENTHESIS:      return "RightParenthesis"; break;
+                case TokenType::END_OF_INPUT:           return "End Of Input"; break;
+                case TokenType::STRING:                 return "String"; break;
+                case TokenType::CHARACTER:              return "Character"; break;
+                case TokenType::SPACE:                  return "Space"; break;
+                case TokenType::NEWLINE:                return "Newline"; break;
+                case TokenType::HASH:                   return "Hash"; break;
+                case TokenType::COLON:                  return "Colon"; break;
+                case TokenType::SEMICOLON:              return "SemiColon"; break;
+                case TokenType::AMPERSAND:              return "Ampersand"; break;
+                case TokenType::DOT:                    return "Dot"; break;
+                case TokenType::COMMA:                  return "Comma"; break;
+                case TokenType::NOT:                    return "Not"; break;
+                case TokenType::LEFT_CURLY_PARENTHESIS: return "Left Curly Parentthesis"; break;
+                case TokenType::RIGHT_CURLY_PARENTHESIS:return "Right Curly Parentthesis"; break;
+                case TokenType::LEFT_BOX_PARENTHESIS:   return "Left Box Parentthesis"; break;
+                case TokenType::RIGHT_BOX_PARENTHESIS:  return "Right Box Parentthesis"; break;
+                case TokenType::UNKNOWN:                return "Unkown"; break;
             }
             return "Unknown";
         }
